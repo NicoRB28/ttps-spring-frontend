0338f49d9033e77d3a2f0436693c6f297a5146b3
nico <nicolasrodriguezboschi@gmail.com>
2020-12-13 14:04:02 -0300

    agrego archivos faltantes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventListComponent } from './modules/event/event-list/event-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"event", component:EventListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ShareModule } from './modules/share/share.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ShareModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
